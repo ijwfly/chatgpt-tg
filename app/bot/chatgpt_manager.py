c0b5d5cc3a77620a70c461e191a0fe93c17c7761
Maxim Nechaev <maxnecha@gmail.com>
2023-11-08 01:09:36 +0300

    Disable streaming answers for correct usage calculation, add ports to docker-compose

--- app/bot/chatgpt_manager.py
+++ app/bot/chatgpt_manager.py
@@ -1,6 +1,6 @@
 from typing import List, AsyncGenerator, Callable
 
-from app.openai_helpers.chatgpt import DialogMessage
+from app.openai_helpers.chatgpt import DialogMessage, GptModel
 from app.storage.db import DB, User
 
 
@@ -10,6 +10,10 @@ class ChatGptManager:
         self.db: DB = db
 
     async def send_user_message(self, user: User, messages: List[DialogMessage], is_cancelled: Callable[[], bool]) -> AsyncGenerator[DialogMessage, None]:
+        if user.current_model == GptModel.GPT_4_VISION_PREVIEW:
+            # TODO: remove this after adding context length calculation for images
+            user.streaming_answers = False
+
         if user.streaming_answers:
             return self.send_user_message_streaming(user, messages, is_cancelled)
         else:
