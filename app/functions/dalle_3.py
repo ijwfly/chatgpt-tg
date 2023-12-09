463b98e1f994eb8ececf436f0e83c82998c04ae7
Maxim Nechaev <maxnecha@gmail.com>
2023-12-10 00:49:52 +0300

    Refactor caption truncation code for Telegram in DALL-E function
    Modified the caption handling logic in the DALL-E function file, dalle_3.py: Captions that exceed Telegram's character limit are now truncated and stored in a separate variable, `tg_caption`, which is then sent to the `send_photo` function. This code clean-up enhances readability and separation of responsibilities within this function.

--- app/functions/dalle_3.py
+++ app/functions/dalle_3.py
@@ -53,9 +53,9 @@ class GenerateImageDalle3(OpenAIFunction):
             caption += resp.data[0].revised_prompt
 
             # truncate caption to 1024 symbols, telegram limit
-            caption = caption[:1021] + '...' if len(caption) > 1024 else caption
+            tg_caption = caption[:1021] + '...' if len(caption) > 1024 else caption
 
-            response = await send_photo(self.message, image_bytes, caption)
+            response = await send_photo(self.message, image_bytes, tg_caption)
             text = caption + '\n\nImage:\n<image.png>'
             dialog_message = DialogUtils.prepare_function_response(self.get_name(), text)
             await self.context_manager.add_message(dialog_message, response.message_id)
